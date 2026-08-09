a815a6d9731d14467510b5af2ca36573e40c2c9f
jayden <jaytan2744@gmail.com>
2026-08-09 19:24:33 -0400

    feat: implement metadata reader

---
+++ src/core/metdata.py
@@ -0,0 +1,9 @@
+from mutagen import File
+
+def main(file):
+    """
+    Get the metadata of a file.
+
+    :return: Release Date, ALbum, Artist, Track Number, Title
+    """
+    return File(file)
