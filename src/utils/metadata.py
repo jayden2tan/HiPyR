d2c62ecdb24adc9d045e400237fdd3a0bad07fc6
jayden <jaytan2744@gmail.com>
2026-08-12 20:01:20 -0400

    .

--- src/utils/metadata.py
+++ src/utils/metadata.py
@@ -3,8 +3,6 @@ from mutagen import File
 
 def get_metadata(file):
     """
-    Get the metadata of a file.
-
     :return: Dictionary with Artist, Track Number, Album, Date, Title
     """
     return File(str(file))
